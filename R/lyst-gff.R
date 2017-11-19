78ba15ef49829908957939506e2ce9566ce51cf3
Liz Cooper <eacooper400@gmail.com>
2017-11-19 00:45:22 -0500

    Added sample data for vcf functions

---
+++ R/lyst-gff.R
@@ -0,0 +1,16 @@
+#' Polar Bear LYST GFF3 
+#'
+#' Gene table for U. maritimus LYST gene region
+#'
+#' @docType data
+#'
+#' @usage data(lyst-gff)
+#'
+#' @format A text file in gff3 format
+#'
+#' @keywords datasets
+#'
+#' @references Liu et al. (2014) Cell 157(4): 785-794
+#'
+#' @examples
+#' makeTxDbFromGFF(file="lyst-gff", format="gff3")
